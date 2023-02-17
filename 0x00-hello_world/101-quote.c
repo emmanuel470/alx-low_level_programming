d72d84dd5cc0ff05df8de2f50c1533cc289ad3d9
emmanuel470 <emmanuelayo00h@gmail.com>
2023-02-16 18:43:09 -0800

    puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unlistd.h>
+/**
+ * main - A program that prints without print or puts
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+
