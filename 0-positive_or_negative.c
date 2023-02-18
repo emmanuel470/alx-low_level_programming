c630172f1fedd3cdf1ad5ad2e3a697c4acaad670
emmanuel470 <emmanuelayo00h@gmail.com>
2023-02-17 20:26:02 -0800

    variable

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 #include <stdio.h>
-
+/* betty style doc for function main goes there */
 /**
- * main - Determine if a number is postiive, negative or zero.
+ * main - main function
  *
- * Return: Always 0 (Success)
+ * Return: always 0 (Success)
  */
 int main(void)
 {
@@ -13,17 +14,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
+		printf("%d is positive", n);
 	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
+		printf("%d is zero", n);
 	else
-	{
-		printf("%d is negative\n", n);
-	}
+		printf("%d is negative", n);
+	printf("\n");
 	return (0);
 }
